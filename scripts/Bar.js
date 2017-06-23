61465162c96ec99586139f440b7e2a68392d8c3f
Ruben Callewaert <rubencallewaertdev@gmail.com>
2017-06-24 00:25:28 +0200

    temporary sorting solution

--- scripts/Bar.js
+++ scripts/Bar.js
@@ -6,6 +6,8 @@ const colors = {
     O: '#abb1e2',
 };
 
+const order = ["P", "D", "O"];
+
 const tooltips = {
     P: 'pool',
     D: 'donations',
@@ -13,12 +15,13 @@ const tooltips = {
 };
 const tooltip = data =>
     Object.keys(data)
+        .sort((a, b) => order.indexOf(a) > order.indexOf(b) ? 1 : -1)
         .map((key, i) => `${data[key]}% ${tooltips[key]}`)
         .join(' / ');
 
 const Bar = data =>
     <div className="bar bar-sm tooltip" data-tooltip={tooltip(data)}>
-        {Object.keys(data).map((key, i) =>{
+        {Object.keys(data).sort((a, b) => order.indexOf(a) > order.indexOf(b) ? 1 : -1).map((key, i) => {
             return <div
                 key={i}
                 className="bar-item"
