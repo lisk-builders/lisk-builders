086baadadc53ad0d008821df0520c7b805991297
Aleksey Popov <alepooop@gmail.com>
2017-06-23 17:29:26 +0300

    Add github button and Affiliation field

--- scripts/Panel.js
+++ scripts/Panel.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import classnames from 'classnames';
 import Bar from './Bar';
+import { GitHub } from './Icons'
 
 const rankClassNames = (rank) =>
   classnames('label float-right', {
     'label-success': rank < 102,
     'label-warning': rank > 101
+  });
+
+const affiliationClassNames = (affiliation) =>
+  classnames('label float-right', {
+    'label-success': affiliation === "Freelance",
+    'label-warning': affiliation !== "Freelance"
   });
 
 const Panel = ({
@@ -19,7 +26,7 @@ const Panel = ({
     affiliation,
     liskChat
 }) =>
-    <div className="column col-xs-12 col-sm-12 col-md-6 col-lg-6">
+    <div key={rank} className="column col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <div className="panel">
             <div className="panel-header text-center">
                 <figure className="avatar avatar-lg" data-initial={delegateName.split('')[0].toUpperCase()}>
@@ -38,6 +45,13 @@ const Panel = ({
                     </div>
                 </div>
                 <div className="divider"></div>
+                <div className="tile tile-centered">
+                    <div className="tile-content">
+                        <span className="tile-title">Affiliation</span>
+                        <label className={affiliationClassNames(affiliation)}>{affiliation}</label>
+                    </div>
+                </div>
+                <div className="divider"></div>
                 <div className="tile">
                   <Bar {...poolPercentage}/>
                 </div>
@@ -47,6 +61,16 @@ const Panel = ({
                         <a className="btn btn-sm btn-block" target="_blank" href={proposal}>Proposal</a>
                     </div>
                 </div>
+                <div className="divider"></div>
+                { githubUsername &&
+                  <div className="tile tile-centered">
+                    <div className="tile-content">
+                        <a className="btn btn-sm btn-block" target="_blank" href={`https://github.com/${githubUsername}`}>
+                          <GitHub /> {githubUsername}
+                        </a>
+                    </div>
+                  </div>
+                }
             </div>
             <div className="panel-footer">
               { liskChat && <a className="btn btn-sm btn-block" target="_blank"href={`https://lisk.chat/direct/${liskChat}`}>@{liskChat}</a> }
