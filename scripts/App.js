cfae0702e099c456cada840a8472d60112762ea9
Ruben Callewaert <rubencallewaertdev@gmail.com>
2017-06-24 14:05:58 +0200

    lotto start

--- scripts/App.js
+++ scripts/App.js
@@ -5,6 +5,7 @@ import Note from './Note';
 import Panel from './Panel';
 import Container from './Container';
 import notes from '../data/notes.json';
+import axios from 'axios';
 
 export default class App extends Component {
     state = {
@@ -32,6 +33,20 @@ export default class App extends Component {
         });
     };
 
+    checkEntered = () => {
+        axios.get(`https://node08.lisk.io/api/accounts/delegates/?address=${this.input.value}`)
+            .then(res => {
+                const neededVotes = this.state.data.length;
+                const relevantVotes = res.data.delegates.filter(delegate => this.state.data.filter((dg) => dg.delegateAddress === delegate.address).length > 0).length;
+                if(relevantVotes === neededVotes) {
+                    alert('OK');
+                } else {
+                    alert('NOK');
+                }
+            })
+            .catch(res => alert('problem'));
+    };
+
     render() {
         const { column, data, direction } = this.state;
         return (
@@ -40,6 +55,12 @@ export default class App extends Component {
                 <div className="bg-gray">
                     <Container>
                         <Note {...notes.note1} />
+                        <ul>
+                            <li>First prize: 1500 LSK</li>
+                            <li>Second prize: 900 LSK</li>
+                            <li>Third prize: 600 LSK</li>
+                        </ul>
+                        <input type="text" ref={(input) => this.input = input}></input><button onClick={this.checkEntered}>Check if you are entered in the lottery</button>
                     </Container>
                 </div>
                 <Container>
