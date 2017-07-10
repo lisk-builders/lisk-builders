cbdb6c5384a65608985ef1ce0cabd75ccc42db61
Aleksey Popov <alepooop@gmail.com>
2017-07-10 17:53:29 +0300

    rewrite lotto script to Ramda just for fun

--- tools/lottery.js
+++ tools/lottery.js
@@ -1,78 +1,128 @@
-const axios = require("axios");
-const delegates = require("../data/delegates.json");
+const axios = require('axios');
+const R = require('ramda');
+const delegates = require('../data/delegates.json');
 
-const lotteryMembers = delegates.filter(dg => dg.lotteryMember);
-const blacklist = lotteryMembers.map(dg => dg.delegateAddress);
+const ROUNDS = 5;
 
-const candidates = {};
+const getRandomArbitrary = R.curry((min, max) =>
+    Math.floor(Math.random() * (max - min) + min)
+);
 
-const getRandomArbitrary = (min, max) => {
-  return Math.random() * (max - min) + min;
-};
+// Alias
+const asyncProgram = R.pipeP;
 
-const getDelegateData = delegate =>
-  axios
-    .get(`https://node02.lisk.io/api/delegates/get?username=${delegate.delegateName}`)
-    .then(res => {
-      return axios.get(`https://node02.lisk.io/api/delegates/voters?publicKey=${res.data.delegate.publicKey}`).then(res2 => {
-        delegate.voters = res2.data.accounts ? res2.data.accounts : undefined;
-        return delegate;
-      });
-    })
-    .catch(res => delegate);
+// Request builders
+const getDelegateRequest = name =>
+    axios.get(`https://node02.lisk.io/api/delegates/get?username=${name}`);
 
-const lotto = entries => {
-  const cleanedEntries = entries.filter(e => e.tickets > 0);
-  const totalTickets = cleanedEntries.reduce((mem, val) => mem + val.tickets, 0);
-  const capTickets = Math.floor(totalTickets / cleanedEntries.length) * 2;
-  console.log("Total Tickets: " + totalTickets);
-  console.log("Average Tickets: " + capTickets);
-  const firstEntries = cleanedEntries.map(acc => {
-    acc.tickets = Math.min(acc.tickets, capTickets);
-    return acc;
-  });
-  const finalTotalTickets = firstEntries.reduce((mem, val) => mem + val.tickets, 0);
-  console.log("Final Total Tickets: " + finalTotalTickets);
-  console.log("Entries: " + firstEntries.length);
-  const firstLotto = [];
-  firstEntries.forEach((entry, index) => {
-    for (let i = 0; i < entry.tickets; i++) {
-      firstLotto.push(index);
-    }
-  });
-  const firstWinnerId = Math.floor(getRandomArbitrary(0, firstLotto.length));
-  const firstWinner = cleanedEntries[firstLotto[firstWinnerId]];
-  console.log("First Prize Winner: " + JSON.stringify(firstWinner));
-  const secondLotto = firstLotto.filter(e => cleanedEntries[e].address !== firstWinner.address);
-  const secondWinnerId = Math.floor(getRandomArbitrary(0, secondLotto.length));
-  const secondWinner = cleanedEntries[secondLotto[secondWinnerId]];
-  console.log("Second Prize Winner: " + JSON.stringify(secondWinner));
-  const thirdLotto = secondLotto.filter(e => [firstWinner.address, secondWinner.address].indexOf(cleanedEntries[e].address) === -1);
-  const thirdWinnerId = Math.floor(getRandomArbitrary(0, thirdLotto.length));
-  const thirdWinner = cleanedEntries[thirdLotto[thirdWinnerId]];
-  console.log("Third Prize Winner: " + JSON.stringify(thirdWinner));
-};
+const getDelegateVotersRequest = publicKey =>
+    axios.get(
+        `https://node02.lisk.io/api/delegates/voters?publicKey=${publicKey}`
+    );
 
-axios.all(lotteryMembers.map(getDelegateData)).then(res => {
-  res.forEach(dg => {
-    dg.voters.forEach(vt => {
-      if (blacklist.indexOf(vt.address) === -1) {
-        if (!candidates[vt.address]) {
-          candidates[vt.address] = 1;
-        } else {
-          candidates[vt.address] += 1;
-        }
-      }
-    });
-  });
-  const validCandidates = Object.keys(candidates).filter(key => candidates[key] === lotteryMembers.length);
-  Promise.all(
-    validCandidates.map(vc => {
-      return axios.get(`https://node02.lisk.io/api/accounts/getBalance?address=${vc}`).then(res2 => {
-        return { address: vc, tickets: Math.floor((res2.data.balance ? res2.data.balance : 0) / 100000000) };
-      });
-    })
-  )
-    .then(lotto)
-    .catch(err => console.log(err));
+// Pick data from response
+const getPublicKey = R.path(['data', 'delegate', 'publicKey']);
+const getAccounts = R.path(['data', 'accounts']);
+
+// "Promise all"
+const getDelegatesByName = R.converge(axios.all, [R.map(getDelegateRequest)]);
+const getDelegateVoters = R.converge(axios.all, [R.map(getDelegateVotersRequest)]);
+
+// Get some lottery members data
+const getLotteryMember = R.filter(R.prop('lotteryMember'));
+const getDelegateName = R.map(R.prop('delegateName'));
+const getDelegateAddress = R.map(R.prop('delegateAddress'));
+
+// Pick delegate data
+const lotteryDelegates = getLotteryMember(delegates);
+const lotteryDelegatesName = getDelegateName(lotteryDelegates);
+const lotteryDelegatesAddress = getDelegateAddress(lotteryDelegates);
+
+// blacklist filter
+const blackList = R.flip(R.contains)(lotteryDelegatesAddress);
+const removeBlackListed = R.pipe(R.prop('address'), blackList);
+
+// Combines two lists into a set (i.e. no duplicates)
+// composed of those elements common to both lists.
+const filterValidAccounts = R.reduce(
+    (acc, el) => (acc.length ? R.intersection(acc, el) : el),
+    []
+);
+
+const normalizeLsk = R.pipe(
+    R.prop('balance'),
+    R.flip(R.divide)(100000000),
+    Math.floor
+);
+
+const prepareBalance = R.converge(R.assoc('balance'), [
+    normalizeLsk,
+    R.identity
+]);
+
+const notZeroBalance = account => account.balance > 0;
+
+// Request data from explorer and prepare for lotto
+const getAndValidateData = asyncProgram(
+    getDelegatesByName,
+    R.map(getPublicKey),
+    getDelegateVoters,
+    R.map(getAccounts),
+    filterValidAccounts,
+    R.reject(removeBlackListed),
+    R.map(prepareBalance),
+    R.filter(notZeroBalance)
+);
+
+// Lotto functions
+const getTotalTickets = R.pipe(R.map(R.prop('balance')), R.sum);
+
+const getFinalTotalTickets = R.pipe(R.map(R.prop('tickets')), R.sum);
+
+const calculateTickets = capTickets =>
+    R.converge(R.assoc('tickets'), [
+        R.pipe(R.prop('balance'), R.min(capTickets)),
+        R.identity
+    ]);
+
+// return list of n length filled with address
+const fillWithAddress = account => R.repeat(account.address, account.tickets);
+// get list filled with lottery member adress
+const generateLottoRange = R.reduce(
+    (acc, el) => R.concat(fillWithAddress(el), acc),
+    []
+);
+
+// Recursivly play round and pick winner addresses
+const playRound = R.curry((round, candidates) => {
+    let winner = candidates[getRandomArbitrary(0, candidates.length)];
+    let newRoundCandidates = R.reject(R.equals(winner), candidates);
+    return round === 0
+        ? []
+        : [].concat(winner, playRound(round - 1, newRoundCandidates));
 });
+
+const getByAddress = (record, id) => record.address === id;
+
+const lotto = accounts => {
+    const totalTickets = getTotalTickets(accounts);
+    const capTickets = Math.floor(totalTickets / accounts.length) * 2;
+    const firstEntries = R.map(calculateTickets(capTickets))(accounts);
+    const finalTotalTickets = getFinalTotalTickets(firstEntries);
+
+    const drawWiners = R.pipe(
+        generateLottoRange,
+        playRound(ROUNDS),
+        R.innerJoin(getByAddress, firstEntries),
+        R.map(R.pick(['address', 'tickets']))
+    );
+
+    console.log('Total Tickets: ' + totalTickets);
+    console.log('Average Tickets: ' + capTickets);
+    console.log('Final Total Tickets: ' + finalTotalTickets);
+    console.log('Entries: ' + firstEntries.length);
+    console.log(drawWiners(firstEntries));
+};
+
+// run
+asyncProgram(getAndValidateData, lotto)(lotteryDelegatesName);
