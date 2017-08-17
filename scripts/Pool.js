cf068c06dbf46624cbb390b90437c5719bbbe118
Aleksey Popov <alepooop@gmail.com>
2017-08-17 14:43:20 +0300

    sort payout data and add bignumber.js

--- scripts/Pool.js
+++ scripts/Pool.js
@@ -6,35 +6,34 @@ import Note from './Note';
 import Panel from './Panel';
 import poolData from '../data/pool.json';
 import notes from '../data/notes.json';
-import { rankClassNames } from './utils';
+import { fromRawLsk } from './utils';
 
-const getForgedLisk = ({publicKey}) =>
+const getForgedLisk = ({ publicKey }) =>
   axios.get(`https://node01.lisk.io/api/delegates/forging/getForgedByAccount?generatorPublicKey=${publicKey}&start=${poolData.updateTimestamp}`)
-  .then(res => res.data); const title = "{ Pool }";
+    .then(res => res.data);
 const getPoolMembers = data => _.filter(data, 'poolMember');
 
 const PoolData = props => (
-  <div className='column col-12'>
-    <div className='panel'>
-      <div className='panel-header text-center'>
-        <div className='panel-title'>Forged from last payout: <b>{props.forged} LSK</b></div>
-        <div className='panel-subtitle'>{ new Date(poolData.updateTimestamp * 1000).toUTCString()}</div>
+  <div className="column col-12">
+    <div className="panel">
+      <div className="panel-header text-center">
+        <div className="panel-title">Forged from last payout: <b>{props.forged} LSK</b></div>
+        <div className="panel-subtitle">{ new Date(poolData.updateTimestamp * 1000).toUTCString()}</div>
       </div>
-      <div className='panel-body'>
-      </div>
+      <div className="panel-body" />
     </div>
   </div>
 );
 
-const Payouts = props => (
-  <div className='column col-12'>
-    <div className='panel'>
-      <div className='panel-header text-center'>
-        <div className='panel-title'> Pool payouts </div>
-        <div className='panel-subtitle'>{ new Date(poolData.updateTimestamp * 1000).toUTCString()}</div>
+const Payouts = () => (
+  <div className="column col-12">
+    <div className="panel">
+      <div className="panel-header text-center">
+        <div className="panel-title"> Pool payouts </div>
+        <div className="panel-subtitle">{ new Date(poolData.updateTimestamp * 1000).toUTCString()}</div>
       </div>
-      <div className='panel-body'>
-        <table className='table table-hover'>
+      <div className="panel-body">
+        <table className="table table-hover">
           <thead>
             <tr>
               <th>Address</th>
@@ -43,11 +42,11 @@ const Payouts = props => (
             </tr>
           </thead>
           <tbody>
-            { poolData.accounts.map((data, i) => (
+            { _.orderBy(poolData.accounts, ['paidBalance'], ['desc']).map((data, i) => (
               <tr key={i}>
                 <td>{ data.address }</td>
-                <td>{ data.paidBalance }</td>
-                <td>{ data.unpaidBalance }</td>
+                <td>{ fromRawLsk(data.paidBalance)}</td>
+                <td>{ fromRawLsk(data.unpaidBalance)}</td>
               </tr>
             ))}
           </tbody>
@@ -55,29 +54,30 @@ const Payouts = props => (
       </div>
     </div>
   </div>
-)
+);
 
 class Pool extends Component {
   constructor(props) {
     super(props);
     this.getTotalForgedLisk = this.getTotalForgedLisk.bind(this);
-    this.state = { forged: 0};
+    this.state = { forged: 0 };
   }
   componentDidMount() {
     const poolMembers = getPoolMembers(this.props.delegates);
     axios.all([...poolMembers.map(getForgedLisk)])
-      .then(this.getTotalForgedLisk);
+      .then(this.getTotalForgedLisk)
+      .catch(() => this.getTotalForgedLisk([]));
   }
   getTotalForgedLisk(data) {
-    const totalForged = data.reduce((acc, el) => acc + el.forged / 100000000, 0)
+    const totalForged = data.reduce((acc, el) => acc + fromRawLsk(el.forged), 0);
     this.setState({
-      forged: parseInt(totalForged * 30 / 100, 10)
+      forged: parseInt((totalForged * 30) / 100, 10)
     });
   }
   render() {
     const delegates = getPoolMembers(this.props.delegates).reverse();
     const activeDelegate = delegates.filter(delegate => delegate.rank <= 101);
-    const progress = 100 * activeDelegate.length / delegates.length;
+    const progress = (100 * activeDelegate.length) / delegates.length;
     return (
       <div>
         <div className="bg-gray">
@@ -86,13 +86,14 @@ class Pool extends Component {
           </Container>
         </div>
         <Container>
-          <PoolData forged={this.state.forged} progress={progress}/>
+          <PoolData forged={this.state.forged} progress={progress} />
           {_.map(delegates, Panel)}
         </Container>
-      <Container>
-        <Payouts />
-      </Container>
+        <Container>
+          <Payouts />
+        </Container>
       </div>
     );
-  } }
-export default Pool
+  }
+}
+export default Pool;
