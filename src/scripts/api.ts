6ec5716790ca2ce8392b72174ccf54358943792f
Ruben Callewaert <5an1ty@users.noreply.github.com>
2019-04-24 17:09:06 +0200

    change api providers

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -2,8 +2,14 @@ import { APIClient } from 'lisk-elements';
 
 export const client = () => {
     return new APIClient([
-        'https://wallet.mylisk.com',
-        'https://wallet.lisknode.io'
+        'https://node01.lisk.io/api/delegates',
+        'https://node02.lisk.io/api/delegates',
+        'https://node03.lisk.io/api/delegates',
+        'https://node04.lisk.io/api/delegates',
+        'https://node05.lisk.io/api/delegates',
+        'https://node06.lisk.io/api/delegates',
+        'https://node07.lisk.io/api/delegates',
+        'https://node08.lisk.io/api/delegates'
     ]);
 }
 
