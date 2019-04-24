4b18080ad8c2dcac9733d5067fa8035c5bc3459e
Ruben Callewaert <5an1ty@users.noreply.github.com>
2019-04-24 17:33:04 +0200

    fix remotes

--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -2,14 +2,14 @@ import { APIClient } from 'lisk-elements';
 
 export const client = () => {
     return new APIClient([
-        'https://node01.lisk.io/api/delegates',
-        'https://node02.lisk.io/api/delegates',
-        'https://node03.lisk.io/api/delegates',
-        'https://node04.lisk.io/api/delegates',
-        'https://node05.lisk.io/api/delegates',
-        'https://node06.lisk.io/api/delegates',
-        'https://node07.lisk.io/api/delegates',
-        'https://node08.lisk.io/api/delegates'
+        'https://node01.lisk.io',
+        'https://node02.lisk.io',
+        'https://node03.lisk.io',
+        'https://node04.lisk.io',
+        'https://node05.lisk.io',
+        'https://node06.lisk.io',
+        'https://node07.lisk.io',
+        'https://node08.lisk.io'
     ]);
 }
 
